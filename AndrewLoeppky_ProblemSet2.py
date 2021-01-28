0b7a529f32e88f0bf88bd74888838daa12fc6819
Andrew Loeppky <Andrew.Loeppky@Gmail.com>
2021-01-28 10:38:41 -0700

    make histograms for ps2

--- AndrewLoeppky_ProblemSet2.py
+++ AndrewLoeppky_ProblemSet2.py
@@ -16,14 +16,14 @@ in_data = pd.read_csv("C:/Users/Owner/UBC_S2021/atmos_chem/problemset2.csv")
 #
 # c) $\Delta V / \Delta log(D_p)$ versus $log(D_p)$
 #
-# So we need to create new columns with $\Delta log(D_p)$, $log(D_p)$, $\Delta S$ and $\Delta V$
+# So we need to create new columns with $\Delta log(D_p)$, $\Delta S$ and $\Delta V$
+# x axis can be done by specifying log scale on the plotting function
 
 # %%
 # create the columns
 in_data["Delta log Dp"] = np.log10(in_data["max size (um)"]) - np.log10(
     in_data["min size (um)"]
 )
-in_data["log Dp"] = np.log10(in_data["Mean of Size int (um)"])
 in_data["Delta S"] = (
     4
     * np.pi
@@ -39,40 +39,57 @@ in_data["Delta V"] = (
 )
 
 in_data
+
 # %%
-# plot for Q1
+# N plot for Q1
 fig1, ax = plt.subplots()
-ax.plot(
-    in_data["Mean of Size int (um)"],
+ax.bar(
+    in_data["min size (um)"],
     in_data["Number of Particles in int"] / in_data["Delta log Dp"],
+    align="edge",
+    width=in_data["max size (um)"] - in_data["min size (um)"],
+    edgecolor="black",
 )
 ax.set_title("Number Distribution with Particle Diameter")
 ax.set_ylabel("$\Delta N / \Delta log(D_p)$")
 ax.set_xlabel("$D_p (\mu m)$")
 ax.set_xscale("log")
+ax.set_xticks([1e-1, 1e0, 1e1])
 
 # %%
 # SA plot for Q3
 fig2, ax = plt.subplots()
-ax.plot(
-    in_data["Mean of Size int (um)"],
+ax.bar(
+    in_data["min size (um)"],
     in_data["Delta S"] / in_data["Delta log Dp"],
+    color="orange",
+    alpha=0.7,
+    align="edge",
+    width=in_data["max size (um)"] - in_data["min size (um)"],
+    edgecolor="black",
 )
 ax.set_title("Surface Area Distribution with Particle Diameter")
 ax.set_ylabel("$\Delta S / \Delta log(D_p)$")
 ax.set_xlabel("$D_p (\mu m)$")
 ax.set_xscale("log")
+ax.set_xticks([1e-1, 1e0, 1e1])
 
 # %%
 # V plot for Q3
 fig3, ax = plt.subplots()
-ax.plot(
-    in_data["Mean of Size int (um)"],
+ax.bar(
+    in_data["min size (um)"],
     in_data["Delta V"] / in_data["Delta log Dp"],
+    color="green",
+    alpha=0.7,
+    align="edge",
+    width=in_data["max size (um)"] - in_data["min size (um)"],
+    edgecolor="black"
 )
 ax.set_title("Volume Distribution with Particle Diameter")
 ax.set_ylabel("$\Delta V / \Delta log(D_p)$")
 ax.set_xlabel("$D_p (\mu m)$")
 ax.set_xscale("log")
+ax.set_xticks([1e-1, 1e0, 1e1])
 
 # %%
